d08d9cefd89ff0e02c1b4b04b8cb9944cc8c314c
Jonathan Verdeli Costa <jonathanverdeli@gmail.com>
2023-03-31 11:11:44 -0300

    [FrontEnd]corrigido requests axios e criado SellerOrder e OrderCard estáticos

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -4,6 +4,7 @@ import Provider from './Components/Provider';
 import Checkout from './Pages/Checkout';
 import Orders from './Pages/Orders';
 import Products from './Pages/Products';
+import SellerOrder from './Pages/SellerOrder';
 import { Login, Register } from './Pages';
 
 function App() {
@@ -19,6 +20,7 @@ function App() {
           <Route exact path="/customer/products" component={ Products } />
           <Route exact path="/customer/orders" component={ Orders } />
           <Route exact path="/customer/checkout" component={ Checkout } />
+          <Route exact path="/seller/orders" component={ SellerOrder } />
         </Switch>
       </Provider>
     </main>
