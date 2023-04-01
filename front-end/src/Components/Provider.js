ce6a55345afbaf48d898e753f4f8e4ed6ee3d272
Jonathan Verdeli Costa <jonathanverdeli@gmail.com>
2023-04-01 15:25:30 -0300

    [Back End] fix: models e migrations

--- front-end/src/Components/Provider.js
+++ front-end/src/Components/Provider.js
@@ -1,14 +1,25 @@
-import React, { /* useEffect */ useMemo, useState } from 'react';
+import React, { useEffect, useMemo, useState } from 'react';
 import PropTypes from 'prop-types';
 import MyContext from '../Context/MyContext';
+import { requestData } from '../services/requests';
 
 function Provider({ children }) {
   const [quantity, setQuantity] = useState(0);
+  const [orders, setOrders] = useState([]);
+
+  useEffect(() => {
+    async function fetchSales() {
+      const { data } = await requestData('/sales');
+      setOrders(data);
+    }
+    fetchSales();
+  }, []);
 
   const contextValue = useMemo(() => ({
     quantity,
     setQuantity,
-  }), [quantity]);
+    orders,
+  }), [quantity, orders]);
 
   return (
     <MyContext.Provider value={ contextValue }>
