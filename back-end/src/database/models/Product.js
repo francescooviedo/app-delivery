acc38c52cab3cae8c31d66fbd6448c633083ad9f
Jonathan Verdeli Costa <jonathanverdeli@gmail.com>
2023-04-03 16:16:38 -0300

    merge migration and models

--- back-end/src/database/models/Product.js
+++ back-end/src/database/models/Product.js
@@ -1,34 +1,36 @@
+
+ 
+
 module.exports = (sequelize, DataTypes) => {
-    const Product = sequelize.define('Product', {
-      id: {
-        allowNull: false, 
-        type: DataTypes.INTEGER, 
-        primaryKey: true, 
-        autoIncrement: true },
-      name: {
-        allowNull: false,
-        type: DataTypes.STRING,
-        unique: true,
-      },
-      price: {
-        allowNull: false,
-        type: DataTypes.DECIMAL(4,2),
-      }, 
-      url_image: {
-        allowNull: false,
-        type: DataTypes.STRING,
-        defaultValue: '',
-      }, 
+  const Product = sequelize.define('Product', {
+    id: {
+      allowNull: false, 
+      type: DataTypes.INTEGER, 
+      primaryKey: true, 
+      autoIncrement: true },
+    name: {
+      allowNull: false,
+      type: DataTypes.STRING,
+      unique: true,
     },
-    {
-      timestamps: false,
-      tableName: 'products',
-      underscored:true
-    }
-    );
-    Product.associate = ({ SaleProduct }) => {
-      Product.hasMany(SaleProduct, { foreignKey: 'productId', as: 'productId' });
-    }
-    
-    return Product;
-  };
+    price: {
+      allowNull: false,
+      type: DataTypes.DECIMAL(4,2),
+    }, 
+    url_image: {
+      allowNull: false,
+      type: DataTypes.STRING,
+      defaultValue: '',
+    }, 
+  },
+  {
+    timestamps: false,
+    modelName: 'products',
+    underscored:true
+  }
+  );
+  Product.associate = ({ SaleProduct }) => {
+    Product.hasMany(SaleProduct, { foreignKey: 'productId', as: 'productId' });
+  }
+  return Product;
+};
