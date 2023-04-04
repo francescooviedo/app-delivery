a9f1bb1085f3fbf51020975ae6827998b01fb97a
francesco oviedo <francesco.oviedo@gmail.com>
2023-04-04 07:50:19 -0300

    projeto vazio

--- back-end/src/api/Router/saleRouter.js
+++ back-end/src/api/Router/saleRouter.js
@@ -1,12 +1,10 @@
 const express = require('express');
 const { createSale } = require('../controllers/saleController');
-const handleTokenAuth = require('../middlewares/handleTokenAuth');
 
 const saleRouter = express.Router();
 
 saleRouter.post(
   '/',
-  handleTokenAuth,
   createSale,
 );
 
--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,29 +1,8 @@
 import React from 'react';
-import { Route, Switch, Redirect } from 'react-router-dom';
-import Provider from './Components/Provider';
-import Checkout from './Pages/Checkout';
-import Orders from './Pages/Orders';
-import Products from './Pages/Products';
-import SellerOrder from './Pages/SellerOrder';
-import { Login, Register } from './Pages';
 
 function App() {
   return (
-    <main>
-      <Provider>
-        <Switch>
-          <Route exact path="/">
-            <Redirect to="/login" />
-          </Route>
-          <Route exact path="/login" component={ Login } />
-          <Route exact path="/register" component={ Register } />
-          <Route exact path="/customer/products" component={ Products } />
-          <Route exact path="/customer/orders" component={ Orders } />
-          <Route exact path="/customer/checkout" component={ Checkout } />
-          <Route exact path="/seller/orders" component={ SellerOrder } />
-        </Switch>
-      </Provider>
-    </main>
+    <main />
   );
 }
 
