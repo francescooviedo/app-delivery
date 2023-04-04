90b807e8360b33c944cc0ab1c05ba5ba3583efdd
francesco oviedo <francesco.oviedo@gmail.com>
2023-04-04 04:40:00 -0300

    1 ao 16 req

--- back-end/src/api/utils/cryptoJWT.js
+++ back-end/src/api/utils/cryptoJWT.js
@@ -46,30 +46,3 @@ module.exports = {
   generateToken,
   comparePassword,
 };
-
-// const secret = await secretKey();
-//         const decryptedData = jwt.verify(authorization, secret);
-//         req.user = decryptedData;
-
-// const jwt = require('jsonwebtoken');
-// const fs = require('fs/promises');
-
-// const secretKey = async () => {
-//   const data = await fs.readFile(
-//     './jwt.evaluation.key', 
-//     'utf-8',
-//   );
-//   return data;
-// };
-
-// const generateToken = async (payload) => {
-//   const options = {
-//     expiresIn: '3d',
-//     algorithm: 'HS256',
-//   };
-//   const key = await secretKey();
-//   const token = jwt.sign(payload, key, options);
-//   return token;
-// };
-
-// module.exports = { generateToken, secretKey };
